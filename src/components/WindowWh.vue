dddcdaaf6ab4c050195e11491bc83544be4eb3f6
fengxiangfei <2778824262@qq.com>
2022-06-22 18:24:36 +0800

    feat:新增获取浏览器窗口大小组件

---
+++ src/components/WindowWh.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+// 获取浏览器窗口大小
+export default () => {
+  const width = ref(window.innerWidth);
+  const height = ref(window.innerHeight);
+
+  const onUpdate = () => {
+    width.value = window.innerWidth;
+    height.value = window.innerHeight;
+  };
+
+  onMounted(() => {
+    window.addEventListener("resize", onUpdate);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("resize", onUpdate);
+  });
+
+  return {
+    width,
+    height,
+  };
+};
+</script>
